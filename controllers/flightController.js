be13675962965dbbd1c5929d6c4718599c2b9513
chikasamuel <samuelmereal@gmail.com>
2022-11-05 09:49:17 +0100

    upload

---
+++ controllers/flightController.js
@@ -0,0 +1,14 @@
+const {flights} = require("../model/User");
+
+exports.creatflight = (req, res) => {
+    flight.push(flight)
+}
+
+exports.getflights = (req, res) => {
+    console.log("All flights")
+    res.send("All Flight")
+}
+
+
+
+
